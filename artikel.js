a9a74f6391a9db101c06af0da453ed8ece6e4ce2
sabinebogers <sb_5050@hotmail.com>
2015-11-05 18:25:07 +0100

    Create artikel.js

---
+++ artikel.js
@@ -0,0 +1,7 @@
+var hamburger = document.getElementById("hamburger");
+var navigatie = document.getElementById("navigatie");
+
+
+    hamburger.onclick = function() {
+            navigatie.classList.toggle("displaynone2");
+    }
